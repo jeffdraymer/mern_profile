598d43c968cea65039e29b14ff6c4fe6178da1fd
Jeff Raymer <jeffdraymer@gmail.com>
2018-09-13 14:51:15 -0400

    can add/remove comments from posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -156,6 +156,69 @@ router.post(
   }
 );
 
+// @route POST /api/posts/comment/:id
+// @desc Add comment to post
+// @access Private
+
+router.post(
+  "/comment/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+
+    Post.findById(req.params.id).then(post => {
+      const newComment = {
+        text: req.body.text,
+        name: req.body.name,
+        avatar: req.body.avatar,
+        user: req.user.id
+      };
+
+      //Add to comments array
+      post.comments.unshift(newComment);
+      post
+        .save()
+        .then(res.json(post))
+        .catch(err => res.status(404).json({ postNotFound: "Post not found" }));
+    });
+  }
+);
+
+// @route Delete /api/posts/comment/:id/:comment_id
+// @desc Delete comment to post
+// @access Private
+
+router.delete(
+  "/comment/:id/:comment_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Post.findById(req.params.id).then(post => {
+      //Check if the comment exists
+      if (
+        (post.comments.filter(
+          comment => comment._id.toString() === req.params.comment_id
+        ).length = 0)
+      ) {
+        return res
+          .status(404)
+          .json({ commentNotFound: "Comment was not found" });
+      }
+
+      const removeIndex = post.comments
+        .map(item => item._id.toString())
+        .indexOf(req.params.comment_id);
+
+      //splice out of the array
+      post.comments.splice(removeIndex, 1);
+      post.save().then(post => res.json(post));
+    });
+  }
+);
+
 // @route DELETE /api/posts/:id
 // @desc Delete post
 // @access Private
