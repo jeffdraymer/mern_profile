ea4885e3c1a152b18815e3bf7070e2544bd6c757
Jeff Raymer <jeffdraymer@gmail.com>
2018-09-13 10:51:28 -0400

    New post model and routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,31 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+const Post = require("../../models/Post");
 
 // @route /api/posts/
 // @desc Tests post route
 // @access Public
 router.get("/", (req, res) => res.json({ message: "posts Works" }));
+
+// @route /api/posts
+// @desc Create post
+// @access Private
+
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const newPost = new Post({
+      text: req.body.text,
+      name: req.body.name,
+      avatar: req.body.avatar,
+      user: req.user.id
+    });
+
+    newPost.save().then(post => res.json(post));
+  }
+);
 
 module.exports = router;
