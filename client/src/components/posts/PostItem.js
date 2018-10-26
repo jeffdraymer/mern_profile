66277a28b877202a8e331c5766352aecbddfd107
root <root@localhost.localdomain>
2018-10-26 13:20:16 -0400

    removing additional bracket

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -42,7 +42,7 @@ class PostItem extends Component {
                             alt="" />
                     </a>
                     <br />
-                    <p className="text-center">{post.name}}</p>
+                    <p className="text-center">{post.name}</p>
                 </div>
                 <div className="col-md-10">
                     <p className="lead">{post.text}</p>
